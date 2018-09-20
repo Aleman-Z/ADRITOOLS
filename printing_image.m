c3656905dd47fd590fa5b07ff40e1464eea9f972
Aleman-Z <r.a.aleman.zapata@student.tue.nl>
2018-09-20 15:44:20 +0200

    print_image

---
+++ printing_image.m
@@ -0,0 +1,14 @@
+function printing_image(name)
+string=strcat(name,'.eps');
+% saveas(gcf,string)
+print(string,'-depsc')
+
+string=strcat(name,'.fig');
+saveas(gcf,string)
+
+string=strcat(name,'.pdf');
+figure_function(gcf,[],string,[]);
+
+string=strcat(name,'.png');
+saveas(gcf,string)
+end
