118458f6fdda6b0ffb7f2aa98c5f3486a820efff
Aleman-Z <r.a.aleman.zapata@student.tue.nl>
2019-01-08 15:28:06 +0100

    im update

--- im.m
+++ im.m
@@ -1,6 +1,14 @@
-function im(o2)
+function im(o2,varargin)
 o2((o2)==0)=NaN;
-% figure()
-J=imagesc(o2);colormap(jet(256)); colorbar()
-set(J,'AlphaData',~isnan(o2))
+varargin;
+    if length(varargin)~=0
+    J=imagesc(varargin{1},varargin{2},o2);colormap(jet(256)); colorbar()
+    set(J,'AlphaData',~isnan(o2))
+    set(gca,'YDir','normal')
+    else
+    % figure()
+    J=imagesc(o2);colormap(jet(256)); colorbar()
+    set(J,'AlphaData',~isnan(o2))
+    set(gca,'YDir','normal')
+    end
 end
