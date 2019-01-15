ad5a3889ac8d5d2c0a2dd0b8e668373c278c7aa4
Aleman-Z <r.a.aleman.zapata@student.tue.nl>
2019-01-15 22:43:06 +0100

    stripes cases

--- stripes.m
+++ stripes.m
@@ -1,9 +1,17 @@
 function stripes(input,a,time)
 if nargin>2
     rrc=area(time,input,'FaceColor','none');
-else
+    alpha(a)
+end
+
+if nargin==2
     rrc=area(input,'FaceColor','none');
+    alpha(a)
+end
+
+if nargin==1
+    rrc=area(input,'FaceColor','none');
+    alpha(0.5)
 end
 set(rrc, 'FaceColor', 'r');
-alpha(a)
 end
