11ad135f9802acb46c87d69139a50d7ff6cb2456
Aleman-Z <r.a.aleman.zapata@student.tue.nl>
2019-03-14 08:00:59 +0100

    whosthis

--- whosthis.m
+++ whosthis.m
@@ -1,3 +1,10 @@
-function whosthis()
-matlab.desktop.editor.getActiveFilename
+function whosthis() %Displays the name of current file opened on editor.
+CurrentFile=matlab.desktop.editor.getActiveFilename;
+for r=length(CurrentFile):-1:1
+   j=CurrentFile(1,r);
+   if strcmp(j,'\')
+       CurrentFile(1,r+1:end-2)
+       break
+   end
+end
 end
