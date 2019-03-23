e50c812d08574ed0e73a0dfc15dbc86b0c0dfc6d
Aleman-Z <r.a.aleman.zapata@student.tue.nl>
2019-03-23 01:14:56 +0100

    messbox update

--- messbox.m
+++ messbox.m
@@ -1,4 +1,4 @@
-function messbox(text,title)
+function[mh]=messbox(text,title)
 mh = msgbox(text, title,'help');     %create msgbox
 th = findall(mh, 'Type', 'Text');                   %get handle to text within msgbox
 th.FontSize = 10;  
