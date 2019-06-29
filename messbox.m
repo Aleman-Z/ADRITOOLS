79044ae56fa18a5dde77146b13e946cf21006803
Aleman-Z <r.a.aleman.zapata@student.tue.nl>
2019-06-29 22:32:09 +0200

    Update messbox.m

--- messbox.m
+++ messbox.m
@@ -1,5 +1,9 @@
 function[mh]=messbox(text,title)
+%[mh]=messbox(text,title)
+%Show a message in a box
+
 mh = msgbox(text, title,'help');     %create msgbox
 th = findall(mh, 'Type', 'Text');                   %get handle to text within msgbox
 th.FontSize = 10;  
+mh.Position=[mh.Position(1) mh.Position(2) 250 mh.Position(4)]
 end
