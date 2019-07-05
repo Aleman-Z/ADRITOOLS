fc8911360773342f1698b6a19c58a5ba0e88a778
Aleman-Z <r.a.aleman.zapata@student.tue.nl>
2019-07-05 21:50:38 +0200

    Update merge_mat.m

--- merge_mat.m
+++ merge_mat.m
@@ -1,7 +1,9 @@
 
 function [z]=merge_mat(x,y)
+%[z]=merge_mat(x,y)
+%Concatenates matrices with different dimensions.
     sx = size(x);
     sy = size(y);
-    a = max(sx(1),sy(1))
-    z = [[x;zeros(abs([a 0]-sx))],[y;zeros(abs([a,0]-sy))]]
+    a = max(sx(1),sy(1));
+    z = [[x;zeros(abs([a 0]-sx))],[y;zeros(abs([a,0]-sy))]];
 end
