e07967307bdd3544a8a868997edfa7d725f8f72b
Aleman-Z <r.a.aleman.zapata@student.tue.nl>
2019-07-18 09:58:58 +0200

    Update stripes.m

--- stripes.m
+++ stripes.m
@@ -1,17 +1,32 @@
-function stripes(input,a,time)
-if nargin>2
+function stripes(input,a,time,colour)
+%stripes(input,a,time)
+%Plots binary data as stripes.
+%Optional inputs:
+%a: Alpha value of transparency.
+%time: time stamps. 
+if nargin==4
     rrc=area(time,input,'FaceColor','none');
     alpha(a)
+    set(rrc, 'FaceColor',colour);
+end
+
+
+if nargin==3
+    rrc=area(time,input,'FaceColor','none');
+    alpha(a)
+    set(rrc, 'FaceColor', 'r');
 end
 
 if nargin==2
     rrc=area(input,'FaceColor','none');
     alpha(a)
+    set(rrc, 'FaceColor', 'r');
 end
 
 if nargin==1
     rrc=area(input,'FaceColor','none');
     alpha(0.5)
+    set(rrc, 'FaceColor', 'r');
 end
-set(rrc, 'FaceColor', 'r');
+
 end
