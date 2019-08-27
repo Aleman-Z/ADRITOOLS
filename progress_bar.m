f311171a41872ff4e3683999c6297b8e6943cf75
Aleman-Z <r.a.aleman.zapata@student.tue.nl>
2019-08-27 03:01:47 +0200

    Update progress_bar.m

--- progress_bar.m
+++ progress_bar.m
@@ -4,12 +4,12 @@ function progress_bar(yourvariable,limitvalue,f)
 %f=waitbar(0,'Please wait...');
 
     %for yourvariable=dam:limitvalue
-    waitbar(yourvariable/limitvalue,f,strcat('Loading',{' '},num2str(yourvariable/limitvalue*100),'%'));    
+    waitbar(yourvariable/limitvalue,f,strcat('Loading',{' '},num2str(round(yourvariable/limitvalue*100,2)),'%'));    
     pause(0.2)
     %end
     %close(f)
     if yourvariable==limitvalue
-    F=waitbar(1,'MISSION PASSED. RESPECT :)')
+    F=waitbar(1,'COMPLETED')
     beep on
     beep
     pause(2)
