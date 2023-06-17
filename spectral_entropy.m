f801716cb7b7d54c47671079a84ef3fe02ee3b23
Aleman-Z <jraleman10@gmail.com>
2023-06-17 13:09:47 +0200

    Update spectral_entropy.m

--- spectral_entropy.m
+++ spectral_entropy.m
@@ -4,13 +4,25 @@ function [en_tropy]=spectral_entropy(signal,f1,f2,fn)
 %f2 upper bound frequency
 %fn sampling rate. 
 
+if isempty(signal)
+    en_tropy=[];
+    return
+end
+
+if size(signal,1)==1
 [pxx,f]=periodogram(signal,[],[],fn);
 %plot(f,pxx)
-
+else
+[pxx,f]=periodogram(signal',[],[],fn); %129 484 
+end
 [Ind1]=findclosest(f,f1); 
 [Ind2]=findclosest(f,f2);
 
-npxx=pxx(Ind1:Ind2)/sum(pxx(Ind1:Ind2));
+if size(signal,1)==1
+npxx=pxx(Ind1:Ind2)/sum(pxx(Ind1:Ind2)); %[22 1]/ [1 1] =[22 1]
+else
+npxx=pxx(Ind1:Ind2,:)./sum(pxx(Ind1:Ind2,:)); %[22 484]/[1 484]
+end
 
 en_tropy=-(sum(npxx.*log2(npxx)));
 
